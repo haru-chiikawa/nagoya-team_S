fcc2b8aa6dfb2096cf2d2b0afb6ab4336c19cd4d
haru <h.ogawa.3014@gmail.com>
2026-03-08 18:17:55 +0900

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,11 +1,12 @@
-fetch("/header.html")
+fetch("header.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
   });
 
-fetch("/footer.html")
+fetch("footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
+
   });
